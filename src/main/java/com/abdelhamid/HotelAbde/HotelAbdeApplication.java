35afcedb79472ecc082dcb9c0317522021bd2fe1
Abdelhamid-Mouloud <abdelhamidmouloudi2003@gmail.com>
2024-10-26 22:06:16 +0100

    initial

---
+++ src/main/java/com/abdelhamid/HotelAbde/HotelAbdeApplication.java
@@ -0,0 +1,13 @@
+package com.abdelhamid.HotelAbde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelAbdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelAbdeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abdelhamid/HotelAbde/HotelAbdeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdelhamid.HotelAbde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelAbdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
